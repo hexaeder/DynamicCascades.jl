457be33a5c0d94d4fd6edd7e1e51d147441708f5
Hans Würfel <git@wuerfel.io>
2021-04-26 16:53:36 +0200

    complete import to metagraph infrastructure

--- src/DynamicCascades.jl
+++ src/DynamicCascades.jl
@@ -3,43 +3,32 @@ module DynamicCascades
 using CSV
 using DataFrames
 using LightGraphs
+using LightGraphs: SimpleGraphs.SimpleEdgeIter
 using MetaGraphs
-using Colors
-import ColorSchemes
-using GraphPlot
-using Random: MersenneTwister
-using Printf
+using Missings
 
 export DATA_DIR
 const DATA_DIR = abspath(@__DIR__, "..", "Data")
 
-export IEEE_Network, load_rts96, import_system, describe_nodes, describe_edges, BusType
+export import_system, describe_nodes, describe_edges, bustype
 
-struct IEEE_Network
-    graph::SimpleGraph{Int}
-    num_of_buses::Int
-    num_of_lines::Int
-    num_of_generators::Int
-    busdict::Dict{Int,Int}
-    bustype::Vector{Int} # 1::Load, 2::Generator, 3::Slack
-    voltage::Vector{Float64}
-    active_power::Vector{Float64}
-    inertia::Vector{Float64}
-    conductance::Vector{Float64}
-    susceptance::Vector{Float64}
-    active_power_coupling::Vector{Float64}
-    emergency_rating::Vector{Float64}
-end
+"""
+    bustype(i::Int)
+    bustype(s::String)
+    bustype(s::Symbol)
 
-@enum BusType Load=1 Generator=2 Slack=3
+Helper to convert the different representations of bus types into eachother.
+Base representation is Symbol:
 
-function MetaGraphs.set_prop!(g, vs::Union{AbstractUnitRange, AbstractArray},
-                              prop::Symbol, vals::Union{AbstractUnitRange, AbstractArray})
-    length(vs) == length(vals) || throw(ArgumentError("vs and vals needs to be of same length!"))
-    for (v, val) in zip(vs, vals)
-        set_prop!(g, v, prop, val)
-    end
-end
+    :load  == bustype(1) == bustype("PQ") == bustype(:PQ)
+    :gen   == bustype(2) == bustype("PV") == bustype(:PV)
+    :slack == bustype(3) == bustype("Ref") == bustype(:Ref)
+"""
+bustype(i::Int) = bustype(Val(i))
+bustype(s::Union{String, Symbol}) = bustype(Val(Symbol(s)))
+bustype(::Union{Val{:PQ},  Val{1}, Val{:load}})  = :load
+bustype(::Union{Val{:PV},  Val{2}, Val{:gen}})   = :gen
+bustype(::Union{Val{:Ref}, Val{3}, Val{:slack}}) = :slack
 
 """
     import_system(sym::Symbol; kwargs...)::MetaGraph
@@ -52,10 +41,10 @@ Main entry point to load the systems. New systems should overload this function.
 Those functions return a `MetaGraph` which has properties attached to the Nodes/Edges.
 
 Graph properties:
-- `BaseP` : Base power for PU
+- `Pbase` : Base power for PU
 optional:
-- `NodeP` : tuple of node property names which show appear first
-- `EdgeP` : tuple of edge property names which show appear first
+- `NodeProps` : tuple of node property names which show appear first
+- `EdgeProps` : tuple of edge property names which show appear first
 
 Node properties:
 - `P` : active power in PU
@@ -74,24 +63,11 @@ Edge properties:
 """
 import_system(sym::Symbol; kwargs...) = import_system(Val(sym); kwargs...)
 
-function describe_nodes(g::MetaGraph; kw=(id=Int[], x=Int[], y=Int[], type=String[]))
-    df = DataFrame(;n=Int[], kw...)
-    for n in 1:nv(g)
-        row = push!(props(g, n), :n=>n)
-        push!(df, row, cols=:union)
-    end
-    df
-end
+"""
+    import_system(:rtsgmlc)
 
-function describe_edges(g::MetaGraph)
-    df = DataFrame(src=Int[], dst=Int[])
-    for e in edges(g)
-        row = push!(props(g, e), :src=>e.src, :dst=>e.dst)
-        push!(df, row, cols=:union)
-    end
-    df
-end
-
+Import the RTS-GMLC system as a MetaGraph.
+"""
 function import_system(::Val{:rtsgmlc})
     @info "Import system RTS-GMLC"
     data = joinpath(DATA_DIR, "RTS-GMLC")
@@ -103,38 +79,45 @@ function import_system(::Val{:rtsgmlc})
     N = nrow(bus_data)
     g = MetaGraph(N)
 
+    baseP = 100
+    set_prop!(g, :Pbase, baseP)
+    set_prop!(g, :NodeProps, [:n, :id, :type, :P, :Q, :inertia, :Vm, :Vbase,
+                              :Va, :P_load, :P_inj, :Q_load, :Q_inj, :x, :y])
+    set_prop!(g, :EdgeProps, [:src, :dst, :R, :X, :rating, :id])
+
     set_prop!(g, 1:N, :id, bus_data."Bus ID")
-    set_prop!(g, 1:N, :V_mag, bus_data."V Mag")
-    set_prop!(g, 1:N, :V_angle, bus_data."V Angle")
+    set_prop!(g, 1:N, :Vm, bus_data."V Mag")
+    set_prop!(g, 1:N, :Va, bus_data."V Angle")
+    set_prop!(g, 1:N, :Vbase, bus_data."BaseKV")
     set_prop!(g, 1:N, :x, bus_data."lng")
     set_prop!(g, 1:N, :y, bus_data."lat")
-    set_prop!(g, 1:N, :type, bus_data."Bus Type")
+    set_prop!(g, 1:N, :type, bustype.(bus_data."Bus Type"))
 
-    set_prop!(g, 1:N, :MW_load, bus_data."MW Load")
-    set_prop!(g, 1:N, :MVAR_load, bus_data."MVAR Load")
+    set_prop!(g, 1:N, :P_load, bus_data."MW Load"/baseP)
+    set_prop!(g, 1:N, :Q_load, bus_data."MVAR Load"/baseP)
 
     for (n, busid) in enumerate(bus_data."Bus ID")
+        # select attached generators
         generators = gen_data[gen_data."Bus ID".== busid, ["Category", "MW Inj", "MVAR Inj", "Inertia MJ/MW"]]
-        mw_inj   = sum(generators."MW Inj")
-        mvar_inj = sum(generators."MVAR Inj")
+        P_inj   = sum(generators."MW Inj") / baseP
+        Q_inj = sum(generators."MVAR Inj") / baseP
         inertia  = sum(generators."Inertia MJ/MW")
-        if mw_inj!=0.0 || mvar_inj!=0.0 || inertia != 0.0
-            set_prop!(g, n, :MW_inj, mw_inj)
-            set_prop!(g, n, :MVAR_inj, mvar_inj)
+        if P_inj!=0.0 || Q_inj!=0.0 || inertia!=0.0
+            set_prop!(g, n, :P_inj, P_inj)
+            set_prop!(g, n, :Q_inj, Q_inj)
             set_prop!(g, n, :inertia, inertia)
         end
-        mw_load = get_prop(g, n, :MW_load)
-        mvar_load = get_prop(g, n, :MVAR_load)
-        set_prop!(g, n, :MW, mw_inj - mw_load)
-        set_prop!(g, n, :MVAR, mvar_inj - mvar_load)
+        P_load = get_prop(g, n, :P_load)
+        Q_load = get_prop(g, n, :Q_load)
+        set_prop!(g, n, :P, P_inj - P_load)
+        set_prop!(g, n, :Q, Q_inj - Q_load)
     end
 
     for row in eachrow(branch_data)
         src = findfirst(x->x==row."From Bus", bus_data."Bus ID")
         dst = findfirst(x->x==row."To Bus", bus_data."Bus ID")
-        # TODO B does not matches the calculated value at all!
         propertys = Dict(:R => row."R", :X => row."X",
-                         :rating => row."STE Rating",
+                         :rating => row."STE Rating"/baseP,
                          :id => row."UID")
         add_edge!(g, src, dst, propertys)
     end
@@ -142,6 +125,11 @@ function import_system(::Val{:rtsgmlc})
     return g
 end
 
+"""
+    import_system(:rts96raw)
+
+Import the RTS96 system from raw data as a MetaGraph.
+"""
 function import_system(::Val{:rts96raw}; losses=false)
     @info "Import system RTS-96 (from raw data)"
     # read data from csv files
@@ -171,53 +159,76 @@ function import_system(::Val{:rts96raw}; losses=false)
     N = nrow(BusData)
     g = MetaGraph(N)
 
+    baseP = 100
+    set_prop!(g, :Pbase, baseP)
+    set_prop!(g, :NodeProps, [:n, :id, :P, :type, :Q, :inertia, :Vm, :Vbase,
+                              :P_load, :P_inj, :Q_load, :Q_inj])
+    set_prop!(g, :EdgeProps, [:src, :dst, :R, :X, :rating, :id])
+
     set_prop!(g, 1:N, :id, BusData.id)
-    set_prop!(g, 1:N, :type, BusData.type)
+    set_prop!(g, 1:N, :type, bustype.(BusData.type))
+    set_prop!(g, 1:N, :Vbase, BusData.base_kV)
 
     for (n, busid) in enumerate(BusData.id)
+        # collect all generators which are attached to bus
         generators = GeneratorData[GeneratorData.:id .== busid, :]
         generators = leftjoin(generators, SystemDynamicsData, on = :unit => :group)
-        generators = generators[:, [:MW_inj, :MVar_inj, :inertia]]
+        generators = generators[:, [:MW_inj, :MVar_inj, :inertia, :V_pu]]
 
-        mw_inj   = sum(generators.:MW_inj)
-        mvar_inj = sum(generators.:MVar_inj)
-        inertia  = sum(generators.:inertia)
+        # extract voltage setpoints in PU, make sure alle generators at bus have same setpoint
+        vpus = unique(generators.V_pu)
+        if length(vpus) == 1
+            set_prop!(g, n, :Vm, vpus[1])
+        elseif length(vpus) != 0
+            error("There are multiple Vpu at single bus!")
+        end
 
-        if mw_inj!=0.0 || mvar_inj!=0.0 || inertia != 0.0
-            # set_prop!(g, n, :MW_inj, mw_inj)
-            # set_prop!(g, n, :MVAR_inj, mvar_inj)
+        # sum up inertia and P/Q of all attached generators
+        P_inj   = sum(generators.MW_inj) / baseP
+        Q_inj = sum(generators.MVar_inj) / baseP
+        inertia  = sum(generators.inertia)
+        if P_inj!=0.0 || Q_inj!=0.0 || inertia != 0.0 # set only if there are generators
+            set_prop!(g, n, :P_inj, P_inj)
+            set_prop!(g, n, :Q_inj, Q_inj)
             set_prop!(g, n, :inertia, inertia)
         end
 
+        # find right bus in busloaddata
         busload = BusLoadData[busid .∈ zip(BusLoadData.bid_1, BusLoadData.bid_2, BusLoadData.bid_3), :]
         if nrow(busload) == 0
-            mw_load = 0.0
-            mvar_load = 0.0
+            P_load = 0.0
+            Q_load = 0.0
         elseif nrow(busload) == 1
-            mw_load = busload.MW[1]
-            mvar_load = busload.MVar[1]
+            P_load = busload.MW[1] / baseP
+            Q_load = busload.MVar[1] / baseP
+            set_prop!(g, n, :P_load, P_load)
+            set_prop!(g, n, :Q_load, Q_load)
+        else
+            error("nrow(busload) = $(nrow(busload)) should be 0 or 1")
         end
 
-        P = (mw_inj - mw_load) / 100
+        P = P_inj - P_load
         set_prop!(g, n, :P, P)
-        Q = (mvar_inj - mvar_load) / 100
+        Q = Q_inj - Q_load
         set_prop!(g, n, :Q, Q)
     end
-
-    # set_prop!(g, 1:N, :V_mag, voltage)
 
     for row in eachrow(BranchData)
         src = findfirst(x->x==row.from, BusData.id)
         dst = findfirst(x->x==row.to, BusData.id)
-        # TODO B does not matches the calculated value at all!
         propertys = Dict(:R => row.R, :X => row.X,
-                         :B => row.B, :rating => row.STE/100,
+                         :rating => row.STE/100,
                          :id => row.id)
         add_edge!(g, src, dst, propertys)
     end
     return g
 end
 
+"""
+    import_system(:rts96prepared)
+
+Import the RTS96 system from the prepared data as a MetaGraph.
+"""
 function import_system(::Val{:rts96prepared}; losses=false)
     @info "Import system RTS-96 (from prepared csv files)"
     # read data from csv files
@@ -235,62 +246,50 @@ function import_system(::Val{:rts96prepared}; losses=false)
 
     # graph structure
     g = MetaGraph(N)
+    # set base parameters
+    baseP = 100
+    set_prop!(g, :Pbase, baseP)
+    set_prop!(g, :NodeProps, [:n, :id, :P, :type, :Q, :inertia, :Vm, :Vbase,
+                              :P_load, :P_inj, :Q_load, :Q_inj])
+    set_prop!(g, :EdgeProps, [:src, :dst, :R, :X, :rating])
+
+    # join all data on bus id
+    joined = leftjoin(BusData, GeneratorData, on=:ID)
+    joined = leftjoin(joined, LoadData, on=:ID)
+    joined = leftjoin(joined, FlowData, on=:ID, makeunique=true)
+    sort!(joined, :ID)
+
+    set_prop!(g, 1:N, :id, joined.ID)
+    set_prop!(g, 1:N, :Vbase, joined.Base_V)
+    set_prop!(g, 1:N, :type, bustype.(joined.Type))
+    set_prop!(g, 1:N, :Vm, joined.Vm)
+    set_prop!(g, 1:N, :Va, joined.Va)
+    set_prop!(g, 1:N, :inertia, joined.Inertia)
+    set_prop!(g, 1:N, :P_load, joined.P_Load)
+    set_prop!(g, 1:N, :Q_load, joined.Q_Load)
+
+    # TODO what ist P_corr?
+    P_inj = losses ? joined.P_Gen : joined.P_Corr
+    @warn "Set Q_inj to zero because there is no data!"
+    Q_inj = zeros(length(P_inj))
+    set_prop!(g, 1:N, :P_inj, P_inj)
+    set_prop!(g, 1:N, :Q_inj, Q_inj)
+
+    # calculate total P and Q
+    P = Missings.replace(P_inj, 0.0) .- joined.P_Load
+    Q = Missings.replace(Q_inj, 0.0) .- joined.Q_Load
+    set_prop!(g, 1:N, :P, P)
+    set_prop!(g, 1:N, :Q, Q)
+
+    # add the edge data
     for line in eachrow(LineData)
         add_edge!(g, line.source, line.dest)
     end
 
-    # busdict connects bus ID form rts96 with internal bus number
-    busdict = Dict{Int64,Int64}()
-    for bus in eachrow(BusData)
-        busdict[bus.ID] = bus.Number
-    end
-
-    # set busid
-    set_prop!(g, 1:N, :id, BusData.ID)
-
-    # bus types & dictionary for bus IDs
-    bustype = convert(Array{Int64}, BusData.Type)
-    set_prop!(g, 1:N, :type, bustype)
-
-    # voltage magnitude
-    voltage = FlowData.Vm
-    set_prop!(g, 1:N, :V_mag, voltage)
-
-    # active power
-    P = - LoadData.P_Load
-    P_Gen = losses ? GeneratorData.P_Gen : GeneratorData.P_Corr
-    for i in 1:M
-        idx = busdict[GeneratorData[i,:ID]]
-        P[idx] += P_Gen[i]
-    end
-    set_prop!(g, 1:N, :P, P)
-
-    # inertia
-    I_Gen = GeneratorData.Inertia
-    inertia = zeros(N)
-    for i in 1:M
-        idx = busdict[GeneratorData[i,:ID]]
-        inertia[idx] += I_Gen[i]
-    end
-    set_prop!(g, 1:N, :inertia, inertia)
-
-    # resistance & reactance
-    X = LineData.x
-    R = losses ? LineData.r : zeros(L)
-    # Y = G + im B, Y=1/Z
-    # TODO added -1 missing in B, is it correct now? seems so!
-    G = R./(R.^2+X.^2)
-    B = -X./(R.^2+X.^2) # calculating the absolute value here
-
-    # caclulate active_power_coupling for each line
-    K = [voltage[e.source] * voltage[e.dest] for e in eachrow(LineData)] .* -1 .* B
-
-    # emergency rating
-    rating = LineData.u
-
-    for (e, r, x, b, rat) in zip(edges(g), R, X, B, rating)
-        set_props!(g, e, Dict(:R=>r, :X=>x, :B=>b, :rating=>rat))
-    end
+    # resistance, reactance & emergency rating
+    set_prop!(g, edges(g), :R, LineData.r)
+    set_prop!(g, edges(g), :X, LineData.x)
+    set_prop!(g, edges(g), :rating, LineData.u)
 
     # check that the orders if edges in graph equals the order of edges in csv
     lineorder1 = [(e.source, e.dest) for e in eachrow(LineData)]
@@ -300,76 +299,71 @@ function import_system(::Val{:rts96prepared}; losses=false)
     return g
 end
 
-function load_rts96(;losses=false)
-    # read data from csv files
-    BusData = CSV.read(joinpath(DATA_DIR,"Bus.csv"), DataFrame)
-    LineData = CSV.read(joinpath(DATA_DIR,"Line.csv"), DataFrame)
-    GeneratorData = CSV.read(joinpath(DATA_DIR,"Generator.csv"), DataFrame)
-    LoadData = CSV.read(joinpath(DATA_DIR,"Load.csv"), DataFrame)
-    FlowData = CSV.read(joinpath(DATA_DIR,"Flow.csv"), DataFrame)
+"""
+    MetaGraphs.set_prop!(g, vs::Iterable, prop::Symbol, vals::Iterable)
 
-    # number of components
-    N = nrow(BusData)
-    L = nrow(LineData)
-    M = nrow(GeneratorData)
-
-    # graph structure
-    g = SimpleGraph(N)
-    for line in eachrow(LineData)
-        add_edge!(g, line.source, line.dest)
+Set same property `prop` with different values `vals` for differet vertices `vs`.
+"""
+function MetaGraphs.set_prop!(g, vs::Union{AbstractUnitRange, AbstractArray},
+                              prop::Symbol, vals::Union{AbstractUnitRange, AbstractArray})
+    length(vs) == length(vals) || throw(ArgumentError("vs and vals needs to be of same length!"))
+    for (v, val) in zip(vs, vals)
+        if !ismissing(val)
+            set_prop!(g, v, prop, val)
+        end
     end
-
-    # busdict connects bus ID form rts96 with internal bus number
-    busdict = Dict{Int64,Int64}()
-    for bus in eachrow(BusData)
-        busdict[bus.ID] = bus.Number
-    end
-
-    # bus types & dictionary for bus IDs
-    bustype = convert(Array{Int64}, BusData.Type)
-
-    # voltage magnitude
-    voltage = FlowData.Vm
-
-    # active power
-    P = - LoadData.P_Load
-    GeneratorData
-    P_Gen = losses ? GeneratorData.P_Gen : GeneratorData.P_Corr
-    for i in 1:M
-        idx = busdict[GeneratorData[i,:ID]]
-        P[idx] += P_Gen[i]
-    end
-
-    # inertia
-    I_Gen = GeneratorData.Inertia
-    inertia = zeros(N)
-    for i in 1:M
-        idx = busdict[GeneratorData[i,:ID]]
-        inertia[idx] += I_Gen[i]
-    end
-
-    # resistance & reactance
-    X = LineData.x
-    R = losses ? LineData.r : zeros(L)
-    # Y = G + im B, Y=1/Z
-    # TODO added -1 missing in B, is it correct now?
-    G = R./(R.^2+X.^2)
-    B = -X./(R.^2+X.^2) # calculating the absolute value here
-
-    # caclulate active_power_coupling for each line
-    K = [voltage[e.source] * voltage[e.dest] for e in eachrow(LineData)] .* -1 .* B
-
-    # emergency rating
-    rating = LineData.u
-
-    # check that the orders if edges in graph equals the order of edges in csv
-    lineorder1 = [(e.source, e.dest) for e in eachrow(LineData)]
-    lineorder2 = [(e.src, e.dst) for e in edges(g)]
-    @assert lineorder1 == lineorder2 "Different order of edges in graph and csv!"
-
-    # constructor
-    IEEE_Network(g, N, L, M, busdict, bustype, voltage, P, inertia, G, B, K, rating)
 end
+
+"""
+    MetaGraphs.set_prop!(g, edges::SImpleEdgeIter, prop::Symbol, vals::Iterable)
+
+Set same property `prop` with different values `vals` for different edges in EdgeIter `edges`.
+"""
+function MetaGraphs.set_prop!(g, edges::SimpleEdgeIter, prop::Symbol, vals)
+    length(edges) == length(vals) || throw(ArgumentError("EdgeIter and vals needs to be of same length!"))
+    for (e, val) in zip(edges, vals)
+        if !ismissing(val)
+            set_prop!(g, e, prop, val)
+        end
+    end
+end
+
+"""
+    describe_nodes(g::MetaGraph; firstcols=Vector{String}())
+
+Returns DataFrame with all the node meta data.
+"""
+function describe_nodes(g::MetaGraph; firstcols=Vector{String}())
+    df = DataFrame(; n=Int[])
+    for n in 1:nv(g)
+        row = push!(props(g, n), :n=>n)
+        push!(df, row, cols=:union)
+    end
+    firstcols = String.(firstcols) # convert to string if given as Symbol
+    has_prop(g, :NodeProps) &&
+        append!(firstcols, String.(get_prop(g, :NodeProps)))
+    append!(firstcols, names(df)) |> unique!
+    select!(df, firstcols)
+end
+
+"""
+    describe_nodes(g::MetaGraph; firstcols=Vector{String}())
+
+Returns DataFrame with all the edge meta data.
+"""
+function describe_edges(g::MetaGraph; firstcols=Vector{String}())
+    df = DataFrame(; src=Int[], dst=Int[])
+    for e in edges(g)
+        row = push!(props(g, e), :src=>e.src, :dst=>e.dst)
+        push!(df, row, cols=:union)
+    end
+    firstcols = String.(firstcols) # convert to string if given as Symbol
+    has_prop(g, :EdgeProps) &&
+        append!(firstcols, String.(get_prop(g, :EdgeProps)))
+    append!(firstcols, names(df)) |> unique!
+    select!(df, firstcols)
+end
+
 
 include("ND_model.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,64 @@
 using Test
 
 using DynamicCascades
+using MetaGraphs
+using Missings
 
-g1 = import_system(Val(:rts96prepared), losses=true)
-g2 = import_system(Val(:rts96raw), losses=true)
-n1 = describe_nodes(g1, (;id=Int[]))
-n2 = describe_nodes(g2, (;id=Int[]))
-e1 = describe_edges(g1)
-e2 = describe_edges(g2)
+@testset "comparison between differen rts systems" begin
+    g1 = import_system(:rtsgmlc)
+    n1 = describe_nodes(g1)
+    e1 = describe_edges(g1)
 
+    g2 = import_system(:rts96raw, losses=true)
+    n2 = describe_nodes(g2)
+    e2 = describe_edges(g2)
 
-# compare the nodes
-nodes = outerjoin(n1, n2, on=:id, makeunique=true)
+    g3 = import_system(:rts96prepared, losses=true)
+    n3 = describe_nodes(g3)
+    e3 = describe_edges(g3)
 
-@test nodes.n == nodes.n_1
-@test nodes.P ≈ nodes.P_1
-@test nodes.type == nodes.type_1
+    @test n1.n == n2.n == n3.n
+    @test n1.id == n2.id == n3.id
+    @test n1.type == n2.type == n3.type
+    @test n1.Vbase == n2.Vbase == n3.Vbase
 
-# FIXME node inertia wrong for node 14?
-nodes.inertia[14] = 0.0 # 114
-nodes.inertia[38] = 0.0 # 214
-nodes.inertia[62] = 0.0 # 314
-@test all(nodes.inertia .≈ Missings.replace(nodes.inertia_1, 0.0))
+    #inertia
+    inert1 = Missings.replace(n1.inertia, 0.0) |> collect
+    inert2 = Missings.replace(n2.inertia, 0.0) |> collect
+    inert3 = Missings.replace(n3.inertia, 0.0) |> collect
+    # Anton sets interita for "Sync Cond" nodes to 1.0
+    inert3[14] = 0.0 # 114
+    inert3[38] = 0.0 # 214
+    inert3[62] = 0.0 # 314
+    @test inert2 ≈ inert3 # same with those 'fixed'
+    Δinert = inert1 .- inert2
+    idx = findall(x->!isapprox(x, 0.0), Δinert)
+    @info "Difference in inert to GMLC" idx .=> Δinert[idx]
 
+    # there are some expected differences in power between 2/3 and 1
+    @test n2.P ≈ n3.P
+    ΔP = n1.P - n2.P
+    idx = findall(x->!isapprox(x, 0.0), n1.P - n2.P)
+    @info "Difference in P to GMLC" idx .=> ΔP[idx]
 
-#compare the edges
-edges = outerjoin(e1, e2, on=[:src, :dst], makeunique=true)
+    # comparison of edges
+    @test e1.src == e2.src == e3.src
+    @test e1.dst == e2.dst == e3.dst
 
-a= findall(x->! isapprox(x, 0.0), edges.rating - edges.rating_1)
-b=findall(x->! isapprox(x, 0.0), edges.X - edges.X_1)
-c=findall(x->! isapprox(x, 0.0), edges.R - edges.R_1)
+    # e1 and e2 is identical for edge data
+    @test e1.R ≈ e2.R
+    @test e1.X ≈ e2.X
+    @test e1.rating ≈ e2.rating
 
-a==b==c
+    idx = [27, 33, 34, 35, 63, 69, 70, 71, 98, 104, 105, 106]
+    a = findall(x->!isapprox(x, 0.0), e2.rating - e3.rating)
+    b = findall(x->!isapprox(x, 0.0), e2.X - e3.X)
+    c = findall(x->!isapprox(x, 0.0), e2.R - e3.R)
+    @test idx == a == b == c
 
-findall(x->! isapprox(x, 0.0), edges.B - edges.B_1)
-
-edges[:,[:rating, :rating_1]]
-
-g3 = import_system(:rtsgmlc)
-describe_nodes(g3)[]
+    # without those weird lines they are all the same
+    wo_idx(vec) = deleteat!(copy(vec), idx)
+    @test wo_idx(e1.R) == wo_idx(e2.R) == wo_idx(e3.R)
+    @test wo_idx(e1.X) == wo_idx(e2.X) == wo_idx(e3.X)
+    @test wo_idx(e1.rating) == wo_idx(e2.rating) == wo_idx(e3.rating)
+end
