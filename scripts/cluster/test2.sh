24419622583443ac626ae95c1ecc1cea9ea04812
kafu16 <24458047+kafu16@users.noreply.github.com>
2023-07-22 19:37:38 +0200

    cluster 6

---
+++ scripts/cluster/test2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --qos=priority
+#SBATCH --time=24:00:00
+#SBATCH --job-name=test2
+#SBATCH --output=test2.out
+#SBATCH --nodes=1
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=brandner@pik-potsdam.de
+
+module purge
+module load julia/1.8.2
+
+julia test2.jl
