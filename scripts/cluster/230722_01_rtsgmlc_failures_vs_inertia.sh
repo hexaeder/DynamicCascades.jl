ffd7c0c1dde7b3a176969415b797548963ef8bcf
kafu16 <24458047+kafu16@users.noreply.github.com>
2023-07-22 18:20:55 +0200

    cluster 3

--- scripts/cluster/230722_01_rtsgmlc_failures_vs_inertia.sh
+++ scripts/cluster/230722_01_rtsgmlc_failures_vs_inertia.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-#SBATCH --qos=priority
-#SBATCH --time=00:10:00
-#SBATCH --job-name=test
-#SBATCH --output=test.out
+#SBATCH --qos=short
+#SBATCH --time=24:00:00
+#SBATCH --job-name=230722_01_rtsgmlc_failures_vs_inertia
+#SBATCH --output=230722_01_rtsgmlc_failures_vs_inertia.out
 #SBATCH --nodes=1
-#SBATCH --mail-type=begin        # send email when job begins
-#SBATCH --mail-type=end          # send email when job ends
-#SBATCH --mail-user=brandner@pik-potsdam.de
+#SBATCH --ntasks-per-node=16
 
-module purge
-module load julia/1.8.2
-
-julia test.jl
+julia -p 16 230722_01_rtsgmlc_failures_vs_inertia.jl
