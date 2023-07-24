689e91cc7398e12cecc09a132cd7ad43478ae9ae
kafu16 <24458047+kafu16@users.noreply.github.com>
2023-07-24 14:38:58 +0200

    scripts inertia vs line/node failures

--- scripts/rtsgmlc_failures_vs_inertia_nodes.jl
+++ scripts/rtsgmlc_failures_vs_inertia_nodes.jl
@@ -10,39 +10,54 @@ using DynamicCascades: PLOT_DIR
 using CairoMakie
 using Dates
 using DataFrames
+using CSV
 
 
 # create folder
 t=now()
 datetime = Dates.format(t, "yyyymmdd_HHMMSS.s") # https://riptutorial.com/julia-lang/example/20476/current-time
-folder = string("/",datetime,"inertia_vs_line_failures")
+folder = string("/",datetime,"inertia_vs_node_failures")
 directory = string(RESULTS_DIR,folder)
 mkpath(directory)
-directory
+
+nd, = nd_model(network)
+ω_state_idxs = idx_containing(nd, "ω")
+gen_node_idxs = map(s -> parse(Int, String(s)[4:end]), nd.syms[ω_state_idxs])
+
+
 damping = 0.1u"s"
 scale_inertia_values = [0.2, 0.5, 1, 1.5, 2, 7, 10, 20] # varying parameter
-rel_number_failures = Float64[]
 df_all_failures = DataFrame()
 for scale_inertia in scale_inertia_values
     network = import_system(:rtsgmlc; damping, scale_inertia, tconst = 0.01u"s")
     number_failures = Float64[]
-    for i in 1:ne(network)
+    for i in gen_node_idxs
         sol = simulate(network;
                        initial_fail = Int[i],
                        init_pert = :line,
                        tspan = (0, 500),
-                       trip_lines = :dynamic,
-                       trip_nodes = :none,
+                       trip_lines = :none,
+                       trip_nodes = :dynamic,
                        trip_load_nodes = :none,
+                       f_min = -2.5,
+                       f_max = 1.5,
                        solverargs = (;dtmax=0.01),
                        verbose = true);
-        push!(number_failures, length(sol.failures.saveval)-1) # `-1` as we don't want to count the initial failure
+        push!(number_failures, length(sol.failures_nodes.saveval))
     end
     df_all_failures[!, string(scale_inertia)] = number_failures
-    # push!(avg_number_failures, mean(number_failures))
-    push!(rel_number_failures, mean(number_failures)/(length(ne(network))-1))
 end
+# write failures for each node/line to .csv
 CSV.write(string(directory,"/all_failures.csv"), df_all_failures)
+
+# calculate relative number of failures
+df_all_failures = DataFrame(CSV.File(string(directory,"/all_failures.csv")))
+rel_number_failures = Float64[]
+for scale_inertia in scale_inertia_values
+    number_failures = df_all_failures[!, string(scale_inertia)]
+    push!(rel_number_failures, mean(number_failures)/length(gen_node_idxs))
+end
+
 
 df_inertia_vs_failures = DataFrame("scale_inertia_values" => scale_inertia_values, "rel_failures" => rel_number_failures)
 CSV.write(string(directory,"/inertia_vs_failures.csv"), df_inertia_vs_failures)
