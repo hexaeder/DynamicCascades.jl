775467fc12983fad625ed46d5531bc1d9af0b9d1
kafu16 <24458047+kafu16@users.noreply.github.com>
2023-07-24 14:30:31 +0200

    cluster 10

--- scripts/cluster/test2.jl
+++ scripts/cluster/test2.jl
@@ -21,31 +21,41 @@ datetime = Dates.format(t, "yyyymmdd_HHMMSS.s") # https://riptutorial.com/julia-
 folder = string("/",datetime,"inertia_vs_line_failures")
 directory = string(RESULTS_DIR,folder)
 mkpath(directory)
-directory
 damping = 0.1u"s"
+# scale_inertia_values = [0.2, 0.5, 1, 1.5, 2, 7, 10, 20] # varying parameter
 scale_inertia_values = [1] # varying parameter
-rel_number_failures = Float64[]
 df_all_failures = DataFrame()
 for scale_inertia in scale_inertia_values
     network = import_system(:rtsgmlc; damping, scale_inertia, tconst = 0.01u"s")
     number_failures = Float64[]
+    # for i in 1:ne(network)
     for i in 1:2
         sol = simulate(network;
                        initial_fail = Int[i],
                        init_pert = :line,
-                       tspan = (0, 50),
+                       tspan = (0, 500),
                        trip_lines = :dynamic,
                        trip_nodes = :none,
                        trip_load_nodes = :none,
+                       f_min = -2.5,
+                       f_max = 1.5,
                        solverargs = (;dtmax=0.01),
                        verbose = true);
         push!(number_failures, length(sol.failures.saveval)-1) # `-1` as we don't want to count the initial failure
     end
     df_all_failures[!, string(scale_inertia)] = number_failures
-    # push!(avg_number_failures, mean(number_failures))
-    push!(rel_number_failures, mean(number_failures)/(length(ne(network))-1))
 end
+# write failures for each node/line to .csv
 CSV.write(string(directory,"/all_failures.csv"), df_all_failures)
+
+# calculate relative number of failures
+df_all_failures = DataFrame(CSV.File(string(directory,"/all_failures.csv")))
+rel_number_failures = Float64[]
+for scale_inertia in scale_inertia_values
+    number_failures = df_all_failures[!, string(scale_inertia)]
+    push!(rel_number_failures, mean(number_failures)/(ne(network)-1))
+end
+
 
 df_inertia_vs_failures = DataFrame("scale_inertia_values" => scale_inertia_values, "rel_failures" => rel_number_failures)
 CSV.write(string(directory,"/inertia_vs_failures.csv"), df_inertia_vs_failures)
