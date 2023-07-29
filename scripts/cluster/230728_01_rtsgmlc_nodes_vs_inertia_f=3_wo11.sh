bdd30399145180856b530cd4c560939814f4b05f
kafu16 <24458047+kafu16@users.noreply.github.com>
2023-07-29 03:10:40 +0200

    cluster 57

--- scripts/cluster/230728_01_rtsgmlc_nodes_vs_inertia_f=3_wo11.sh
+++ scripts/cluster/230728_01_rtsgmlc_nodes_vs_inertia_f=3_wo11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --qos=priority
-#SBATCH --time=1-00:00:00
+#SBATCH --qos=medium
+#SBATCH --time=2-00:00:00
 #SBATCH --job-name=230728_01_rtsgmlc_nodes_vs_inertia_f=3_wo11
 #SBATCH --output=230728_01_rtsgmlc_nodes_vs_inertia_f=3_wo11.out
 #SBATCH --nodes=1
