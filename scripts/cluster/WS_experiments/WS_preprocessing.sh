e30394cce2a75a6334b082cc83fb91b9b6c5609f
kafu16 <24458047+kafu16@users.noreply.github.com>
2024-01-01 18:36:15 +0100

    cl 83

--- scripts/cluster/WS_experiments/WS_preprocessing.sh
+++ scripts/cluster/WS_experiments/WS_preprocessing.sh
@@ -4,19 +4,18 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --job-name=test_preprocessing
 #SBATCH --account=icone
-#SBATCH --output=bla.out
+#SBATCH --output=%x-%j-%N.out
+#SBATCH --error=%x-%j-%N.err
 
 
-
-
-echo "Check whether new commits need to be pulled"
-echo ""
-/home/brandner/DynamicCascades.jl/scripts/cluster/WS_experiments/slurm_git.sh
+# # TODO This is more complicated as this requires internet.
+# echo "Check whether new commits need to be pulled"
+# module load git
+# git pull
 
 
 
 module purge
 module load julia/1.8.2
 
-julia test_job.jl
-# julia WS_preprocessing.jl
+julia WS_preprocessing.jl
