d8fdd3c8d2cbb88c866ba983d97ad04e7d784c19
kafu16 <24458047+kafu16@users.noreply.github.com>
2024-01-01 17:05:04 +0100

    cluster 81

--- scripts/cluster/WS_experiments/test_job.jl
+++ scripts/cluster/WS_experiments/test_job.jl
@@ -1,4 +1,6 @@
-bla_string = ARGS[1]
-println(typeof(bla_string))
+# bla_string = ARGS[1]
+# println(typeof(bla_string))
 # bla_symbol = Symbol(bla_string)
 # println(typeof(bla_symbol))
+
+println("Hello git.")
