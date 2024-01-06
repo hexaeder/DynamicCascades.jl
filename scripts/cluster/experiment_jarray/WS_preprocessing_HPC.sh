31cdfe1a5e007f510bc787c94eb087dc67dfa466
kafu16 <24458047+kafu16@users.noreply.github.com>
2024-01-07 00:04:42 +0100

    cl 14

--- scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
+++ scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --qos=priority
-#SBATCH --time=1-00:00:00
+#SBATCH --time=0-01:00:00
 #SBATCH --job-name=test_preprocessing
 #SBATCH --account=icone
 #SBATCH --output=%x-%j-%N.out
