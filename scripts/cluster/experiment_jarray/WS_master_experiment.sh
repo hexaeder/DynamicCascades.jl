8e40654b8ff36ad678561eada6789d8cd9ff4fb2
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-01-20 16:14:44 +0100

    testrun2

--- scripts/cluster/experiment_jarray/WS_master_experiment.sh
+++ scripts/cluster/experiment_jarray/WS_master_experiment.sh
@@ -3,9 +3,9 @@
 ############################## Parameters to be chosen #########################
 name=WS_k=4_exp03_I_over_Dsq_test_
 # inertia_values = [0.2, 0.5, 1.0, 3.0, 5.0, 7.5, 10.0, 20.0, 30.0]
-qos_array=(short short short)
-times_array=(0-05:00:00 0-07:00:00 0-09:00:00)
-cpus_array=(1 1 1)
+qos_array=(short short)
+times_array=(0-05:00:00 0-07:00:00)
+cpus_array=(1 1)
 
 ############################## Preprocessing ###################################
 echo "------------------------------------------------------------"
