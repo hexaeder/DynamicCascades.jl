f14854a5b2b527cf36b7dbc89c1f65f563a48d81
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-01-24 12:39:59 +0100

    c10

--- scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
+++ scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --qos=priority
 #SBATCH --time=1-00:00:00
-#SBATCH --job-name=preprocessing_WS_k=4_exp05_3_I_over_Dsq_lines_and_nodes_
+#SBATCH --job-name=preprocessing_WS_k=4_exp06_1_I_over_Dsq_lines__
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 
@@ -12,4 +12,4 @@
 
 # julia WS_preprocessing_I_over_Dsq.jl
 
-/home/brandner/tmpjulia/julia-1.8.4/bin/julia WS_preprocessing_I_over_Dsq_lines_and_nodes.jl
+/home/brandner/tmpjulia/julia-1.8.4/bin/julia WS_preprocessing_I_over_D_lines.jl
