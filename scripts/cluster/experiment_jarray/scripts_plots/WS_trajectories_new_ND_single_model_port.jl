de6aa3aff72585a321ca26d5a3bd87561c43c861
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-04-12 13:13:45 +0200

    before adapting affect in order to to decide whether only model or only Pmech is changed

--- scripts/cluster/experiment_jarray/scripts_plots/WS_trajectories_new_ND_single_model_port.jl
+++ scripts/cluster/experiment_jarray/scripts_plots/WS_trajectories_new_ND_single_model_port.jl
@@ -5,6 +5,7 @@ using OrdinaryDiffEq
 # using OrdinaryDiffEqTsit5
 using MetaGraphs
 
+
 @mtkmodel DynLoad begin
     @variables begin
         θ(t) = 0.0, [description = "Voltage angle", output=true]
@@ -184,7 +185,9 @@ end
 function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
     verbose=true,
     failtime=0.1,
-    tspan=(0., 100.),
+    tspan=(0., 100000.),
+    trip_lines=:dynamic,
+    trip_nodes=:dynamic,
     terminate_steady_state=true,
     solverargs=(;),
     warn=true)
@@ -196,6 +199,7 @@ function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
 
     # load parameters
     N,k,β,graph_seed,μ,σ,distr_seed,K,α,M,γ,τ,freq_bound,trip_lines,trip_nodes,init_pert,ensemble_element = get_network_args_stripped(df_config, task_id)
+
 
     # load network
     network = import_system_wrapper(df_config, task_id)
@@ -221,6 +225,10 @@ function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
     ### build NetworkDynamics.jl Network
     ###
 
+    # set failure modes
+    trip_nodes == :dynamic ? ωmax = freq_bound*2π : ωmax = Inf
+    trip_lines == :dynamic ? rating = α*K : rating = Inf 
+
     # loop over vertices and assign vertex models & parameter
     vm_array = VertexModel[]
     for i in 1:nv(network)
@@ -232,7 +240,7 @@ function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
 
         type = get_prop(network, i, :type)
         if type == :gen
-            vm = SwingDynLoadModel(M=M,D=γ,τ=τ,ωmax=freq_bound*2π,Pmech=Pmech,Pload=Pload)
+            vm = SwingDynLoadModel(M=M,D=γ,τ=τ,ωmax=ωmax,Pmech=Pmech,Pload=Pload)
         elseif type == :load
             vm = DynLoadModel(τ=τ,Pload=Pload)  
         end
@@ -242,7 +250,7 @@ function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
     end
 
     # generate `Network` object
-    nw = Network(graph, vm_array, Line(K=K,rating=α*K); dealias=true)
+    nw = Network(graph, vm_array, Line(K=K,rating=rating); dealias=true)
 
     # Check if network is power balanced
     nw_state = NWState(nw)
@@ -292,4 +300,75 @@ function simulate_new_ND_single_model_port(exp_data_dir, task_id, initial_fail;
     end
 
     return sol, nw
+end
+
+
+function plot_simulation(simulation_dir, plot_dir, df_config, task_id, initial_fail)
+    sol = Serialization.deserialize(joinpath(simulation_dir, "task_id=$task_id,initial_fail=$initial_fail.sol"));
+    nw = Serialization.deserialize(joinpath(simulation_dir, "task_id=$task_id,initial_fail=$initial_fail.nw"));
+
+    # calculate indices of failing lines and nodes
+    all_failing_nodes_idxs = [i for i in 1:nv(nw) if sol(sol.t[end], idxs=vidxs(i, :functional))[1] == 0]
+    all_failing_lines_idxs = [i for i in 1:ne(nw) if sol(sol.t[end], idxs=eidxs(i, :active))[1] == 0]
+    node_colors_failing = distinguishable_colors(length(all_failing_nodes_idxs))
+    line_colors_failing = distinguishable_colors(length(all_failing_lines_idxs))
+
+    all_nodes_idxs = [i for i in 1:nv(nw)]
+    all_lines_idxs = [i for i in 1:ne(nw)]
+    node_colors = distinguishable_colors(length(all_nodes_idxs))
+    line_colors = distinguishable_colors(length(all_lines_idxs))
+
+    ################################################################################
+    ############################ Line and nodes ####################################
+    ################################################################################
+    fontsize = 35
+    titlesize = (fontsize+5)
+    linewidth = 3.5
+    fig = Figure(size=(3100,1500), fontsize= fontsize)
+    # Add a global title in the first row spanning all columns.
+    xlim = sol.t[end]/3
+
+
+
+    N,k,β,graph_seed,μ,σ,distr_seed,K,α,M,γ,τ,freq_bound,trip_lines,trip_nodes,init_pert,ensemble_element = get_network_args_stripped(df_config, task_id)
+
+    # FREQUENCIES ########################################################################
+    fig[1,1] = ax = Axis(fig; ylabel="Frequency [Hz]", title="Lines & nodes that fail (left column), all lines & nodes (right column):  I=$M,D=$γ,τ=$τ,f_b=$freq_bound,α=$α,K=$K,N=$N,k=$k,β=$β,ensemble element=$ensemble_element", titlealign = :left, titlesize = titlesize)
+    for i in all_failing_nodes_idxs
+        x, y = remove_zero_tail!(deepcopy(sol.t), deepcopy(map(first, sol(sol.t, idxs=vidxs(i, :f)))))
+        color = node_colors_failing[findfirst(x -> x == i, all_failing_nodes_idxs)]
+        lines!(ax, x, y; label="Node $i", color=color, linewidth=linewidth) # consider plotdensity=Int(1e5)
+        scatter!(ax, (x[end], y[end]); color=color, marker=:star5, markersize=25)
+    end
+
+    fig[1,2] = ax = Axis(fig; titlealign = :left, titlesize = titlesize)
+    for i in all_nodes_idxs
+        x, y = remove_zero_tail!(deepcopy(sol.t), deepcopy(map(first, sol(sol.t, idxs=vidxs(i, :f)))))
+        color = node_colors[findfirst(x -> x == i, all_nodes_idxs)]
+        lines!(ax, x, y; label="Node $i", color=color, linewidth=linewidth) # consider plotdensity=Int(1e5)
+        scatter!(ax, (x[end], y[end]); color=color, marker=:star5, markersize=25)
+    end
+    xlims!(ax, 0, xlim)
+
+    # FLOWS ########################################################################
+    fig[2,1] = ax = Axis(fig; xlabel="Time [s]", ylabel="App. power flow [p.u.]")
+    for i in all_failing_lines_idxs
+        x, y = remove_zero_tail!(deepcopy(sol.t), deepcopy(map(first, sol(sol.t, idxs=eidxs(i, :S)))))
+        color = line_colors_failing[findfirst(x -> x == i, all_failing_lines_idxs)]
+        lines!(ax, x, y; label="Node $i", color=color, linewidth=linewidth) # consider plotdensity=Int(1e5)
+        scatter!(ax, (x[end], y[end]); color=color, marker=:star5, markersize=25)
+    end
+
+    fig[2,2] = ax = Axis(fig; xlabel="Time [s]")
+    for i in all_lines_idxs
+        x, y = remove_zero_tail!(deepcopy(sol.t), deepcopy(map(first, sol(sol.t, idxs=eidxs(i, :S)))))
+        color = line_colors[findfirst(x -> x == i, all_lines_idxs)]
+        lines!(ax, x, y; label="Node $i", color=color, linewidth=linewidth) # consider plotdensity=Int(1e5)
+        scatter!(ax, (x[end], y[end]); color=color, marker=:star5, markersize=25)
+    end
+    xlims!(ax, 0, xlim)
+
+    CairoMakie.save(joinpath(plot_dir, "ensemble_element=$ensemble_element,I=$M,D=$γ,f_b=$freq_bound,task_id=$task_id,initial_fail=$initial_fail.pdf"),fig)
+    CairoMakie.save(joinpath(plot_dir, "ensemble_element=$ensemble_element,I=$M,D=$γ,f_b=$freq_bound,task_id=$task_id,initial_fail=$initial_fail.png"),fig)
+    # fig
 end
