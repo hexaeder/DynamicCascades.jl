357ee8f8e6972c520db0e0a7b997397e26d62b17
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-04-19 15:17:51 +0200

    Pmech

--- scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
+++ scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --qos=priority
 #SBATCH --time=1-00:00:00
-#SBATCH --job-name=WS_k=4_exp11_vary_I_only_lines_and_nodes_change_to_BH_
+#SBATCH --job-name=WS_k=4_exp12_vary_I_only_lines_and_nodes_change_Pmech_
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 
@@ -12,4 +12,4 @@
 
 # julia WS_preprocessing_I_over_Dsq.jl
 
-/home/brandner/tmpjulia/julia-1.8.4/bin/julia WS_preprocessing_vary_I_only_lines_and_nodes_change_to_BH.jl
+/home/brandner/tmpjulia/julia-1.8.4/bin/julia WS_preprocessing_vary_I_only_lines_and_nodes_change_Pmech.jl
