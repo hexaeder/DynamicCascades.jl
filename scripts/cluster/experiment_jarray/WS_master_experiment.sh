8568a73a6da18b8ccbec41c92054be9ca380b956
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-04-19 14:27:09 +0200

    bla

--- scripts/cluster/experiment_jarray/WS_master_experiment.sh
+++ scripts/cluster/experiment_jarray/WS_master_experiment.sh
@@ -3,13 +3,9 @@
 ############################## Parameters to be chosen #########################
 name=WS_k=4_exp11_vary_I_only_lines_and_nodes_change_to_BH_
 # inertia_values = [0.2, 0.5, 1.0, 3.0, 5.0, 7.5, 10.0, 20.0, 30.0]
-# qos_array=(short short short short short short short short short)
-# times_array=(0-05:00:00 0-07:00:00 0-09:00:00 0-11:00:00 0-15:00:00 0-20:00:00 0-22:00:00 0-24:00:00 0-24:00:00)
-# cpus_array=(1 1 1 1 1 1 1 1 1)
-
-qos_array=(short short short short short short short medium medium)
-times_array=(0-05:00:00 0-07:00:00 0-09:00:00 0-11:00:00 0-15:00:00 0-20:00:00 0-22:00:00 1-12:00:00 2-00:00:00)
-cpus_array=(1 1 1 1 1 1 1 2 2)
+qos_array=(short short short short short short short short short)
+times_array=(0-05:00:00 0-07:00:00 0-09:00:00 0-11:00:00 0-15:00:00 0-20:00:00 0-22:00:00 0-24:00:00 0-24:00:00)
+cpus_array=(1 1 1 1 1 1 1 1 1)
 
 ############################## Preprocessing ###################################
 echo "------------------------------------------------------------"
@@ -39,10 +35,6 @@ cp -r /home/brandner/DynamicCascades.jl $workdir
 
 # NOTE bash starts at index 0.
 for job_array_index in $(seq 1 1 $N_inertia); do
-    if [[ $job_array_index -eq 9 ]]; then
-        echo "Pausing for 100 seconds before job_array_index $N_inertia to manage submission load."
-        sleep 100
-    fi
     echo "Job array index $job_array_index"
     qos=${qos_array[($job_array_index-1)]}
     time=${times_array[($job_array_index-1)]}
