fd13e2bcac2ebc8e08e153573f082a0eacdbc8fd
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-04-20 14:25:08 +0200

    complement f_b BH

--- scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
+++ scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --qos=priority
 #SBATCH --time=1-00:00:00
-#SBATCH --job-name=WS_k=4_exp12_vary_I_only_lines_and_nodes_change_Pmech_complement_f_b
+#SBATCH --job-name=WS_k=4_exp11_vary_I_only_lines_and_nodes_change_to_BH_complement_f_b
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 
