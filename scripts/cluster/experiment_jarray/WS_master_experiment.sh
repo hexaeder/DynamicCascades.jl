863546eea16747af1b3b21e866a99e8ec3236009
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-04-20 14:20:29 +0200

    complement bounds Pmech

--- scripts/cluster/experiment_jarray/WS_master_experiment.sh
+++ scripts/cluster/experiment_jarray/WS_master_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############################## Parameters to be chosen #########################
-name=WS_k=4_exp12_vary_I_only_lines_and_nodes_change_Pmech_
+name=WS_k=4_exp12_vary_I_only_lines_and_nodes_change_Pmech_complement_f_b
 # inertia_values = [0.2, 0.5, 1.0, 3.0, 5.0, 7.5, 10.0, 20.0, 30.0]
 qos_array=(short short short short short short short short short)
 times_array=(0-05:00:00 0-07:00:00 0-09:00:00 0-11:00:00 0-15:00:00 0-20:00:00 0-22:00:00 0-24:00:00 0-24:00:00)
