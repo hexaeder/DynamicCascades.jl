bd9c678e085850c92a9321d48069534a2cc9dce1
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-05-31 11:52:21 +0200

    added `nd_model_and_CB_new_ND`, adapted `plot_simulation` for RTS

--- src/ND_model_new_ND.jl
+++ src/ND_model_new_ND.jl
@@ -1,4 +1,5 @@
 using NetworkDynamics
+using Unitful: @u_str
 using ModelingToolkit
 using ModelingToolkit: D_nounits as Dt, t_nounits as t
 using OrdinaryDiffEq
@@ -6,9 +7,14 @@ using OrdinaryDiffEq
 using SciMLNLSolve
 using MetaGraphs
 
+
 # export simulate_new_ND
 # export SwingDynLoadModel, SwingDynLoadModel_change_to_BH_only, SwingDynLoadModel_change_Pmech_only
 # export plot_simulation
+
+###
+### Models
+###
 
 @mtkmodel DynLoad begin
     @variables begin
@@ -237,7 +243,8 @@ end
 # end
 
 # checks if LHS of ODE is close to zero.
-function issteadystate_new_ND(nw, x_static)
+function issteadystate_new_ND(network, x_static)
+    nw = nd_model_and_CB_new_ND(network;)
     dx = similar(x_static)
     nw(dx, x_static, pflat(NWState(nw)), 0.0) # Rate of change (derivative) of each state variable storing it in `dx`.
     return maximum(abs.(dx))
@@ -249,9 +256,13 @@ end
 - `zeroidx::Integer=nothing`: If this flag is set, this shifts the phase angle
 at all nodes by the phase angle of the node with index `zeroidx`.
 """
-function steadystate_new_ND(nw; verbose=false, tol=1e-7, zeroidx=nothing) 
+function steadystate_new_ND(network; verbose=false, tol=1e-7, zeroidx=nothing) 
     verbose && println("Find steady state...")
 
+    #= The CBs do not affect the steady state of the system.
+    This is why the default kwargs of `nd_model_and_CB_new_ND` are set to `trip_lines = :none` 
+    and `trip_nodes = :none` which implies `ωmax = Inf` and `rating = Inf`.=#
+    nw = nd_model_and_CB_new_ND(network;)
     s = NWState(nw)
     p = pflat(s)
     x0 = solve(SteadyStateProblem(nw, uflat(s), p), NLSolveJL()) # `uflat(s)` creates vector of zeros
@@ -276,7 +287,7 @@ function steadystate_new_ND(nw; verbose=false, tol=1e-7, zeroidx=nothing)
         error("Steadystate: θ ∈ $ex, consider projecting into [-π,π]!")
     end
 
-    residuum = issteadystate_new_ND(nw, x_static)
+    residuum = issteadystate_new_ND(network, x_static)
     @assert residuum < tol "No steady state found: maximum(abs.(dx))=$residuum"
 
     return x_static 
@@ -285,93 +296,40 @@ end
 
 """
 Choice of ODE_solver 
+the order and number of failures doesnt change with increased precision, 
+i.e. reltol=1e-4 instead of default value reltol=1e-3
+# BUG remove
     Rodas4P() might be better for WS expriment
     AutoTsit5(Rosenbrock23()) might be better for RTS experiment 
     AutoTsit5(Rodas5P()) might be better for RTS experiment 
 """
 function simulate_new_ND(network;
-    verbose=true,
-    graph=network.graph, 
-    gen_model=SwingDynLoadModel,
-    x_static=nothing,
-    zeroidx = 1, #HACK does not take effect for `x_static!=nothing`
-    initial_fail=1,
-    failtime=0.1,
-    tspan=(0., 100000.),
-    trip_lines = :dynamic,
-    trip_nodes = :dynamic,
-    freq_bound = 1.0,
-    terminate_steady_state=true,
-    ODE_solver = AutoTsit5(Rodas5P()),
-    solverargs=(;),
-    warn=true
-    )
+        verbose=true,
+        graph=network.graph,
+        gen_model=SwingDynLoadModel,
+        x_static=steadystate_new_ND(network; verbose, zeroidx=1),
+        initial_fail=Int[], # multiple failures at once implented, e.g. initial_fail=[1,27]
+        failtime=0.1,
+        tspan=(0., 100000.),
+        trip_lines = :dynamic,
+        trip_nodes = :dynamic,
+        freq_bound = 1.0,
+        terminate_steady_state=true,
+        ODE_solver = AutoTsit5(Rodas5P()),
+        solverargs=(;),
+        warn=true
+        )
+
+    nw = nd_model_and_CB_new_ND(network; 
+            graph=graph,
+            gen_model=gen_model,
+            trip_lines=trip_lines,
+            trip_nodes=trip_nodes,
+            freq_bound=freq_bound)
 
     ###
-    ### get parameters
+    ### checks
     ### 
-    set_inertia!(network) # for the WS network it does not add node parameters
-    set_admittance!(network) # NOTE this is needed for WS network as well!
-
-    ###
-    ### build NetworkDynamics.jl Network
-    ###
-
-    # set node failure mode
-    trip_nodes == :dynamic ? ωmax = freq_bound*2π : ωmax = Inf # This is a global node property
-
-    # loop over vertices and assign vertex models & parameter
-    vm_array = VertexModel[]
-    for i in 1:nv(network)
-        # P = P_inj - P_load see `balance_power!`; P_inj = Pmech
-        # P_inj = P + P_load
-        P = ustrip(u"pu", get_prop(network, i, :P))
-        Pload = ustrip(u"pu", get_prop(network, i, :P_load))
-        Pmech = P + Pload
-        τ = ustrip(u"s", get_prop(network, i, :timeconst))
-        type = get_prop(network, i, :type)
-        if type == :gen || type == :syncon
-            M = ustrip(u"s^2", get_prop(network, i, :_M))
-            γ = ustrip(u"s", get_prop(network, i, :damping))
-            vm = gen_model(M=M,D=γ,τ=τ,ωmax=ωmax,Pmech=Pmech,Pload=Pload)
-        elseif type == :load
-            vm = DynLoadModel(τ=τ,Pload=Pload) 
-        else
-            error("The node type :$type is not defined!")
-        end
-        # set positions for plotting
-        set_position!(vm, get_prop(network, i, :pos))
-        push!(vm_array, vm)
-    end
-
-    # loop over edges and assign edge parameters
-    em_array = EdgeModel[]
-    for e in edges(network)
-        # set coupling: K = srcV * dstV / X = - srcV * dstV * imag(Y) , with 1/X = - Im(Y), X: reactance
-        srcV = ustrip(u"pu", get_prop(network, e.src, :Vm))
-        dstV = ustrip(u"pu", get_prop(network, e.dst, :Vm))
-        Y = get_prop(network, e, :_Y)
-        K = - srcV * dstV * imag(Y)
-        set_prop!(network, e, :_K, K)
-        Yabs = abs(Y)
-        # set line failure mode and rating
-        trip_lines == :dynamic ? rating = ustrip(u"pu", get_prop(network, e, :rating)) : rating = Inf 
-        em = Line(srcV=srcV, dstV=dstV, K=K, Yabs=Yabs, rating=rating)
-        push!(em_array, em)
-    end
-
-    # generate `Network` object
-    nw = Network(graph, vm_array, em_array)
-
-    # set initial perturbation CB
-    init_perturb = PresetTimeComponentCallback(failtime,
-        ComponentAffect([], [:active]) do u, p, ctx
-            println("Shutdown line $(ctx.eidx) at t=$(ctx.integrator.t)")
-            p[:active] = 0
-        end
-    )
-    set_callback!(nw[EIndex(initial_fail)], init_perturb)
-
 
     # Check if network is power balanced (this is also checked before creating `nw` when importing the MetaGraph network in import_rtsgmlc.jl)
     p = NWParameter(nw)
@@ -379,18 +337,28 @@ function simulate_new_ND(network;
         sum(p.v[map(idx -> idx.compidx, vpidxs(nw, :, "Pload")), :Pload]),
         sum(p.v[map(idx -> idx.compidx, vpidxs(nw, :, "Pmech")), :Pmech]))
 
-
-    ###
-    ### calculate initial steady state and check initial overloads
-    ###    
-    x_static = isnothing(x_static) ? steadystate_new_ND(nw; verbose, zeroidx=zeroidx) : x_static
-    # s0=find_fixpoint(nw) #NOTE RTS: ERROR: SingularException(1)
+    # check if initial loads exceed rating (this could also be done directly in the CB (s. MM HW [2025-03-24 Mo]))  
     s0 = NWState(nw, x_static, pflat(NWParameter(nw)))
-
-    # check if initial loads exceed rating (it is also possible to do this check directly in the CB (s. MM HW [2025-03-24 Mo]))
     for i in 1:ne(nw)
         if s0[EIndex(i, :S)] > s0[EIndex(i, :rating)]
             error("At least line $i exceeds the apparent power rating.")
+        end
+    end
+
+    ###
+    ### set initial perturbation CB
+    ###
+    if !isempty(initial_fail)
+        # set initial perturbation CB
+        init_perturb = PresetTimeComponentCallback(failtime,
+            ComponentAffect([], [:active]) do u, p, ctx
+                println("Shutdown line $(ctx.eidx) at t=$(ctx.integrator.t)")
+                p[:active] = 0
+            end
+        )
+        # set_callback!(nw[EIndex(initial_fail)], init_perturb)
+        for i in initial_fail
+            set_callback!(nw[EIndex(i)], init_perturb)
         end
     end
 
@@ -460,6 +428,73 @@ function simulate_new_ND(exp_name_date, task_id, initial_fail; kwargs...)
     return sol
 end
 
+function nd_model_and_CB_new_ND(network::MetaGraph;
+        graph=network.graph,
+        gen_model=SwingDynLoadModel,
+        trip_lines = :none,
+        trip_nodes = :none,
+        freq_bound = 1.0
+        )
+
+    ###
+    ### get parameters
+    ### 
+    set_inertia!(network) # for the WS network it does not add node parameters
+    set_admittance!(network) # NOTE this is needed for WS network as well!
+
+    ###
+    ### build NetworkDynamics.jl Network
+    ###
+
+    # set node failure mode
+    trip_nodes == :dynamic ? ωmax = freq_bound*2π : ωmax = Inf # This is a global node property
+
+    # loop over vertices and assign vertex models & parameter
+    vm_array = VertexModel[]
+    for i in 1:nv(network)
+        # P = P_inj - P_load see `balance_power!`; P_inj = Pmech
+        # P_inj = P + P_load
+        P = ustrip(u"pu", get_prop(network, i, :P))
+        Pload = ustrip(u"pu", get_prop(network, i, :P_load))
+        Pmech = P + Pload
+        τ = ustrip(u"s", get_prop(network, i, :timeconst))
+        type = get_prop(network, i, :type)
+        if type == :gen || type == :syncon
+            M = ustrip(u"s^2", get_prop(network, i, :_M))
+            γ = ustrip(u"s", get_prop(network, i, :damping))
+            vm = gen_model(M=M,D=γ,τ=τ,ωmax=ωmax,Pmech=Pmech,Pload=Pload)
+        elseif type == :load
+            vm = DynLoadModel(τ=τ,Pload=Pload) 
+        else
+            error("The node type :$type is not defined!")
+        end
+        # set positions for plotting
+        set_position!(vm, get_prop(network, i, :pos))
+        push!(vm_array, vm)
+    end
+
+    # loop over edges and assign edge parameters
+    em_array = EdgeModel[]
+    for e in edges(network)
+        # set coupling: K = srcV * dstV / X = - srcV * dstV * imag(Y) , with 1/X = - Im(Y), X: reactance
+        srcV = ustrip(u"pu", get_prop(network, e.src, :Vm))
+        dstV = ustrip(u"pu", get_prop(network, e.dst, :Vm))
+        Y = get_prop(network, e, :_Y)
+        K = - srcV * dstV * imag(Y)
+        set_prop!(network, e, :_K, K)
+        Yabs = abs(Y)
+        # set line failure mode and rating
+        trip_lines == :dynamic ? rating = ustrip(u"pu", get_prop(network, e, :rating)) : rating = Inf 
+        em = Line(srcV=srcV, dstV=dstV, K=K, Yabs=Yabs, rating=rating)
+        push!(em_array, em)
+    end
+
+    # generate `Network` object
+    return nw = Network(graph, vm_array, em_array)
+end
+
+
+
 
 using Colors
 # TODO adapt for RTS
@@ -467,12 +502,13 @@ function plot_simulation(sol)
     nw = NetworkDynamics.extract_nw(sol)
 
     # calculate indices of failing lines and nodes
-    all_failing_nodes_idxs = [i for i in 1:nv(nw) if sol(sol.t[end], idxs=vidxs(i, :functional))[1] == 0]
+    idxs_init_swing = map(idx -> idx.compidx, vidxs(nw, :, "ω")) # indices that are initially swing nodes
+    all_failing_nodes_idxs = [i for i in idxs_init_swing if sol(sol.t[end], idxs=vidxs(i, :functional))[1] == 0]
     all_failing_lines_idxs = [i for i in 1:ne(nw) if sol(sol.t[end], idxs=eidxs(i, :active))[1] == 0]
     node_colors_failing = distinguishable_colors(length(all_failing_nodes_idxs))
     line_colors_failing = distinguishable_colors(length(all_failing_lines_idxs))
 
-    all_nodes_idxs = [i for i in 1:nv(nw)]
+    all_nodes_idxs = [i for i in idxs_init_swing]
     all_lines_idxs = [i for i in 1:ne(nw)]
     node_colors = distinguishable_colors(length(all_nodes_idxs))
     line_colors = distinguishable_colors(length(all_lines_idxs))
@@ -485,12 +521,17 @@ function plot_simulation(sol)
     linewidth = 3.5
     fig = Figure(size=(3100,1500), fontsize=fontsize)
     # Add a global title in the first row spanning all columns.
-    xlim = sol.t[end]/3
+    # xlim = sol.t[end]/3
+    xlim = sol.t[end]
 
-    N,k,β,graph_seed,μ,σ,distr_seed,K,α,M,γ,τ,freq_bound,trip_lines,trip_nodes,init_pert,ensemble_element = get_network_args_stripped(df_config, task_id)
+    # TODO probaly move to plot_simulation(simulation_dir, plot_dir, task_id, initial_fail) using if branch for "WS"
+    #  and adapt title below in fig[1,1] with ternary operator or so
+    # N,k,β,graph_seed,μ,σ,distr_seed,K,α,M,γ,τ,freq_bound,trip_lines,trip_nodes,init_pert,ensemble_element = get_network_args_stripped(df_config, task_id)
+    # Maybe even get parameters from `sol`
 
     # FREQUENCIES ########################################################################
-    fig[1,1] = ax = Axis(fig; ylabel="Frequency [Hz]", title="Lines & nodes that fail (left column), all lines & nodes (right column):  I=$M,D=$γ,τ=$τ,f_b=$freq_bound,α=$α,K=$K,N=$N,k=$k,β=$β,ensemble element=$ensemble_element", titlealign = :left, titlesize = titlesize)
+    # fig[1,1] = ax = Axis(fig; ylabel="Frequency [Hz]", title="Lines & nodes that fail (left column), all lines & nodes (right column):  I=$M,D=$γ,τ=$τ,f_b=$freq_bound,α=$α,K=$K,N=$N,k=$k,β=$β,ensemble element=$ensemble_element", titlealign = :left, titlesize = titlesize)
+    fig[1,1] = ax = Axis(fig; ylabel="Frequency [Hz]", title="Lines & nodes that fail (left column), all lines & nodes (right column)", titlealign = :left, titlesize = titlesize)
     # NOTE #TODO This works now: `sol(sol.t, idxs=vidxs(1:4, :ω))`
     for i in all_failing_nodes_idxs
         x, y = remove_zero_tail!(deepcopy(sol.t), deepcopy(map(first, sol(sol.t, idxs=vidxs(i, :f)))))
@@ -532,6 +573,7 @@ end
 function plot_simulation(simulation_dir, plot_dir, task_id, initial_fail)
     sol = Serialization.deserialize(joinpath(simulation_dir, "task_id=$task_id,initial_fail=$initial_fail.sol"));
     fig = plot_simulation(sol)
+    # BUG read in parameters here or get them from `sol` 
     CairoMakie.save(joinpath(plot_dir, "ensemble_element=$ensemble_element,I=$M,D=$γ,f_b=$freq_bound,task_id=$task_id,initial_fail=$initial_fail.pdf"),fig)
     CairoMakie.save(joinpath(plot_dir, "ensemble_element=$ensemble_element,I=$M,D=$γ,f_b=$freq_bound,task_id=$task_id,initial_fail=$initial_fail.png"),fig)
 end
