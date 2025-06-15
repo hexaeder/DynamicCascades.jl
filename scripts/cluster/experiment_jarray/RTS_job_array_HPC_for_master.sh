56f0ae631644b95ae2d838b5754f63a5b859390b
kafu16 <24458047+kafu16@users.noreply.github.com>
2025-06-15 10:47:05 +0200

    copied scripts for experiments from `mwe_old_ND_maybe_plots` to main

---
+++ scripts/cluster/experiment_jarray/RTS_job_array_HPC_for_master.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=icone
+#SBATCH --output=%x-%A_%a-%N.out
+#SBATCH --error=%x-%A_%a-%N.err
+#SBATCH --ntasks=1
+
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-type=FAIL         # send email when job fails
+#SBATCH --mail-user=brandner@pik-potsdam.de
+
+exp_name_date=$1 # getting variable from master script
+job_array_index=$2
+
+cd /home/brandner/DynamicCascades.jl/scripts/cluster/experiment_jarray
+
+module purge
+module load julia/1.8.2
+
+julia RTS_job.jl $SLURM_ARRAY_TASK_ID $exp_name_date $job_array_index
---
+++ scripts/cluster/experiment_jarray/WS_preprocessing_HPC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --qos=priority
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=WS_k=4_exp11_vary_I_only_lines_and_nodes_change_to_BH_complement_f_b
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+
+# module purge
+# module load julia/1.8.2
+
+# julia WS_preprocessing_I_over_Dsq.jl
+
+/home/brandner/tmpjulia/julia-1.8.4/bin/julia WS_preprocessing_vary_I_only_lines_and_nodes_change_to_BH.jl
